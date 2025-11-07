9d405f9d325857bd26e8342fb69eac74f60448cb
SatyaMasonite <147715297+SatyaMasonite@users.noreply.github.com>
2025-11-08 00:02:54 +0530

    fourth commit

--- function.py
+++ function.py
@@ -9,10 +9,18 @@
 
 # greet_user("Bitt")
 
-def avg_cal(val1,val2=12):
-    avg= (val1 + val2)/2
-    return avg
+# def avg_cal(val1,val2=12):
+#     avg= (val1 + val2)/2
+#     return avg
 
-a=avg_cal(10)
-print(a)
+# a=avg_cal(10)
+# print(a)
+# print(a)
+
+#recursive
+def factorial(n): 
+    if (n==1 or n==0):
+        return 1
+    return n * factorial(n - 1)
+a=factorial(5)
 print(a)
